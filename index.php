2efc3790e78e735966a9a0c369b4e8ff5849ac37
kDevelop <kat-kole@mail.ru>
2020-05-20 12:52:31 +0300

    top product fixes

--- index.php
+++ index.php
@@ -258,128 +258,113 @@ $APPLICATION->SetPageProperty('keywords', '');
         );?>
     </div>
 </section>
-<section class="pt-5 pb-5">
-    <div class="container">
-        <div class="h2 mb-5">
-            <?$APPLICATION->IncludeComponent(
-                "bitrix:main.include",
-                ".default",
-                [
-                    "AREA_FILE_SHOW" => "file",
-                    "PATH" => SITE_TEMPLATE_PATH . "/include/index/catalog_top-title.php"
-                ],
-                false
-            );?>
-        </div>
-        <?
-        $lineElemCnt = 4;
-        if (DEVICE_TYPE == "TABLET") {
-            $lineElemCnt = 2;
-        } elseif (DEVICE_TYPE == "MOBILE") {
-            $lineElemCnt = 1;
-        }
-        $GLOBALS["arCatalogTopFilter"] = [
-            "PROPERTY_SHOW_IN_MAIN_VALUE" => "Y"
-        ];
-        $APPLICATION->IncludeComponent(
-            "bitrix:catalog.top",
-            "",
-            [
-                "ACTION_VARIABLE" => "action",
-                "ADD_PICT_PROP" => "MORE_PHOTO",
-                "ADD_PROPERTIES_TO_BASKET" => "Y",
-                "ADD_TO_BASKET_ACTION" => "ADD",
-                "BASKET_URL" => "/personal/basket.php",
-                "BRAND_PROPERTY" => "BRAND_REF",
-                "CACHE_FILTER" => "N",
-                "CACHE_GROUPS" => "Y",
-                "CACHE_TIME" => "36000000",
-                "CACHE_TYPE" => "A",
-                "COMPARE_NAME" => "CATALOG_COMPARE_LIST",
-                "COMPARE_PATH" => "",
-                "COMPATIBLE_MODE" => "N",
-                "CONVERT_CURRENCY" => "Y",
-                "CURRENCY_ID" => "RUB",
-                "CUSTOM_FILTER" => "",
-                "DATA_LAYER_NAME" => "dataLayer",
-                "DETAIL_URL" => "",
-                "DISCOUNT_PERCENT_POSITION" => "bottom-right",
-                "DISPLAY_COMPARE" => "N",
-                "ELEMENT_COUNT" => 10,
-                "ELEMENT_SORT_FIELD" => "sort",
-                "ELEMENT_SORT_FIELD2" => "id",
-                "ELEMENT_SORT_ORDER" => "asc",
-                "ELEMENT_SORT_ORDER2" => "desc",
-                "ENLARGE_PRODUCT" => "STRICT",
-                "FILTER_NAME" => "arCatalogTopFilter",
-                "HIDE_NOT_AVAILABLE" => "Y",
-                "HIDE_NOT_AVAILABLE_OFFERS" => "N",
-                "IBLOCK_ID" => IBLOCK_1C_CATALOG_CATALOG,
-                "IBLOCK_TYPE" => "1c_catalog",
-                "LABEL_PROP" => array("SALELEADER"),
-                "LABEL_PROP_MOBILE" => array(),
-                "LABEL_PROP_POSITION" => "top-left",
-                "LINE_ELEMENT_COUNT" => $lineElemCnt,
-                "MESS_BTN_ADD_TO_BASKET" => "Купить",
-                "MESS_BTN_BUY" => "Купить",
-                "MESS_BTN_COMPARE" => "Сравнить",
-                "MESS_BTN_DETAIL" => "Подробнее",
-                "MESS_NOT_AVAILABLE" => "Нет в наличии",
-                "MESS_RELATIVE_QUANTITY_FEW" => "мало",
-                "MESS_RELATIVE_QUANTITY_MANY" => "много",
-                "MESS_SHOW_MAX_QUANTITY" => "Наличие",
-                "OFFERS_CART_PROPERTIES" => array("COLOR_REF","SIZES_SHOES","SIZES_CLOTHES"),
-                "OFFERS_FIELD_CODE" => array("ID", "NAME", "PREVIEW_PICTURE", "CODE"),
-                "OFFERS_LIMIT" => "0",
-                "OFFERS_PROPERTY_CODE" => array("size_length", "size_width", "size_height", "sleep_size_length", "sleep_size_width", "sleep_size_height"),
-                "OFFERS_SORT_FIELD" => "sort",
-                "OFFERS_SORT_FIELD2" => "id",
-                "OFFERS_SORT_ORDER" => "asc",
-                "OFFERS_SORT_ORDER2" => "desc",
-                "OFFER_ADD_PICT_PROP" => "MORE_PHOTO",
-                "OFFER_TREE_PROPS" => array("COLOR_REF","SIZES_SHOES"),
-                "PARTIAL_PRODUCT_PROPERTIES" => "N",
-                "PRICE_CODE" => array(PRICE_CODE),
-                "PRICE_VAT_INCLUDE" => "Y",
-                "PRODUCT_BLOCKS_ORDER" => "price,buttons",
-                "PRODUCT_DISPLAY_MODE" => "Y",
-                "PRODUCT_ID_VARIABLE" => "id",
-                "PRODUCT_PROPERTIES" => array("NEWPRODUCT"),
-                "PRODUCT_PROPS_VARIABLE" => "prop",
-                "PRODUCT_QUANTITY_VARIABLE" => "",
-                "PRODUCT_ROW_VARIANTS" => "[{'VARIANT':'2','BIG_DATA':false},{'VARIANT':'2','BIG_DATA':false},{'VARIANT':'2','BIG_DATA':false}]",
-                "PRODUCT_SUBSCRIPTION" => "Y",
-                "PROPERTY_CODE" => array(),
-                "PROPERTY_CODE_MOBILE" => array(),
-                "RELATIVE_QUANTITY_FACTOR" => "5",
-                "ROTATE_TIMER" => "30",
-                "SECTION_URL" => "",
-                "SEF_MODE" => "N",
-                "SEF_RULE" => "",
-                "SHOW_CLOSE_POPUP" => "N",
-                "SHOW_DISCOUNT_PERCENT" => "Y",
-                "SHOW_MAX_QUANTITY" => "N",
-                "SHOW_OLD_PRICE" => "Y",
-                "SHOW_PAGINATION" => "Y",
-                "SHOW_PRICE_COUNT" => "1",
-                "SHOW_SLIDER" => "Y",
-                "SLIDER_INTERVAL" => "3000",
-                "SLIDER_PROGRESS" => "N",
-                "TEMPLATE_THEME" => "blue",
-                "USE_ENHANCED_ECOMMERCE" => "Y",
-                "USE_PRICE_COUNT" => "N",
-                "USE_PRODUCT_QUANTITY" => "Y",
-                "VIEW_MODE" => "SECTION",
-                "SLIDER_ARROWS" => DEVICE_TYPE == "DESKTOP" ? "true" : "false",
-                "SLIDER_AUTOPLAY" => DEVICE_TYPE == "DESKTOP" ? "false" : "true",
-                "IMAGE_SIZE" => [
-                    "WIDTH" => 300,
-                    "HEIGHT" => 300
-                ]
-            ]
-        );?>
-    </div>
-</section>
+<?
+$lineElemCnt = 4;
+if (DEVICE_TYPE == "TABLET") {
+    $lineElemCnt = 2;
+} elseif (DEVICE_TYPE == "MOBILE") {
+    $lineElemCnt = 1;
+}
+$GLOBALS["arCatalogTopFilter"] = [
+    "PROPERTY_SHOW_IN_MAIN_VALUE" => "Y"
+];
+$APPLICATION->IncludeComponent(
+    "bitrix:catalog.top",
+    "",
+    [
+        "ACTION_VARIABLE" => "action",
+        "ADD_PICT_PROP" => "MORE_PHOTO",
+        "ADD_PROPERTIES_TO_BASKET" => "Y",
+        "ADD_TO_BASKET_ACTION" => "ADD",
+        "BASKET_URL" => "/personal/basket.php",
+        "BRAND_PROPERTY" => "BRAND_REF",
+        "CACHE_FILTER" => "N",
+        "CACHE_GROUPS" => "Y",
+        "CACHE_TIME" => "36000000",
+        "CACHE_TYPE" => "A",
+        "COMPARE_NAME" => "CATALOG_COMPARE_LIST",
+        "COMPARE_PATH" => "",
+        "COMPATIBLE_MODE" => "N",
+        "CONVERT_CURRENCY" => "Y",
+        "CURRENCY_ID" => "RUB",
+        "CUSTOM_FILTER" => "",
+        "DATA_LAYER_NAME" => "dataLayer",
+        "DETAIL_URL" => "",
+        "DISCOUNT_PERCENT_POSITION" => "bottom-right",
+        "DISPLAY_COMPARE" => "N",
+        "ELEMENT_COUNT" => 10,
+        "ELEMENT_SORT_FIELD" => "sort",
+        "ELEMENT_SORT_FIELD2" => "id",
+        "ELEMENT_SORT_ORDER" => "asc",
+        "ELEMENT_SORT_ORDER2" => "desc",
+        "ENLARGE_PRODUCT" => "STRICT",
+        "FILTER_NAME" => "arCatalogTopFilter",
+        "HIDE_NOT_AVAILABLE" => "Y",
+        "HIDE_NOT_AVAILABLE_OFFERS" => "N",
+        "IBLOCK_ID" => IBLOCK_1C_CATALOG_CATALOG,
+        "IBLOCK_TYPE" => "1c_catalog",
+        "LABEL_PROP" => array("SALELEADER"),
+        "LABEL_PROP_MOBILE" => array(),
+        "LABEL_PROP_POSITION" => "top-left",
+        "LINE_ELEMENT_COUNT" => $lineElemCnt,
+        "MESS_BTN_ADD_TO_BASKET" => "Купить",
+        "MESS_BTN_BUY" => "Купить",
+        "MESS_BTN_COMPARE" => "Сравнить",
+        "MESS_BTN_DETAIL" => "Подробнее",
+        "MESS_NOT_AVAILABLE" => "Нет в наличии",
+        "MESS_RELATIVE_QUANTITY_FEW" => "мало",
+        "MESS_RELATIVE_QUANTITY_MANY" => "много",
+        "MESS_SHOW_MAX_QUANTITY" => "Наличие",
+        "OFFERS_CART_PROPERTIES" => array("COLOR_REF","SIZES_SHOES","SIZES_CLOTHES"),
+        "OFFERS_FIELD_CODE" => array("ID", "NAME", "PREVIEW_PICTURE", "CODE"),
+        "OFFERS_LIMIT" => "0",
+        "OFFERS_PROPERTY_CODE" => array("size_length", "size_width", "size_height", "sleep_size_length", "sleep_size_width", "sleep_size_height"),
+        "OFFERS_SORT_FIELD" => "sort",
+        "OFFERS_SORT_FIELD2" => "id",
+        "OFFERS_SORT_ORDER" => "asc",
+        "OFFERS_SORT_ORDER2" => "desc",
+        "OFFER_ADD_PICT_PROP" => "MORE_PHOTO",
+        "OFFER_TREE_PROPS" => array("COLOR_REF","SIZES_SHOES"),
+        "PARTIAL_PRODUCT_PROPERTIES" => "N",
+        "PRICE_CODE" => array(PRICE_CODE),
+        "PRICE_VAT_INCLUDE" => "Y",
+        "PRODUCT_BLOCKS_ORDER" => "price,buttons",
+        "PRODUCT_DISPLAY_MODE" => "Y",
+        "PRODUCT_ID_VARIABLE" => "id",
+        "PRODUCT_PROPERTIES" => array("NEWPRODUCT"),
+        "PRODUCT_PROPS_VARIABLE" => "prop",
+        "PRODUCT_QUANTITY_VARIABLE" => "",
+        "PRODUCT_ROW_VARIANTS" => "[{'VARIANT':'2','BIG_DATA':false},{'VARIANT':'2','BIG_DATA':false},{'VARIANT':'2','BIG_DATA':false}]",
+        "PRODUCT_SUBSCRIPTION" => "Y",
+        "PROPERTY_CODE" => array(),
+        "PROPERTY_CODE_MOBILE" => array(),
+        "RELATIVE_QUANTITY_FACTOR" => "5",
+        "ROTATE_TIMER" => "30",
+        "SECTION_URL" => "",
+        "SEF_MODE" => "N",
+        "SEF_RULE" => "",
+        "SHOW_CLOSE_POPUP" => "N",
+        "SHOW_DISCOUNT_PERCENT" => "Y",
+        "SHOW_MAX_QUANTITY" => "N",
+        "SHOW_OLD_PRICE" => "Y",
+        "SHOW_PAGINATION" => "Y",
+        "SHOW_PRICE_COUNT" => "1",
+        "SHOW_SLIDER" => "Y",
+        "SLIDER_INTERVAL" => "3000",
+        "SLIDER_PROGRESS" => "N",
+        "TEMPLATE_THEME" => "blue",
+        "USE_ENHANCED_ECOMMERCE" => "Y",
+        "USE_PRICE_COUNT" => "N",
+        "USE_PRODUCT_QUANTITY" => "Y",
+        "VIEW_MODE" => "SECTION",
+        "SLIDER_ARROWS" => DEVICE_TYPE == "DESKTOP" ? "true" : "false",
+        "SLIDER_AUTOPLAY" => DEVICE_TYPE == "DESKTOP" ? "false" : "true",
+        "IMAGE_SIZE" => [
+            "WIDTH" => 300,
+            "HEIGHT" => 300
+        ]
+    ]
+);?>
 <section class="pt-5 pb-5">
     <div class="container">
         <div class="h2 mb-5">
--- local/templates/.default/components/bitrix/catalog.top/.default/template.php
+++ local/templates/.default/components/bitrix/catalog.top/.default/template.php
@@ -17,40 +17,45 @@ $wrapClass = $arResult["ITEMS_COUNT"] > $arParams["LINE_ELEMENT_COUNT"] ? ' js-s
 ?>
 
 <?if ($arResult["ITEMS_COUNT"] > 0) :?>
-    <div
-        class="row clearfix<?=$wrapClass?>"
-        data-autoplay="<?=$arParams["SLIDER_AUTOPLAY"]?>"
-        data-autoplaySpeed="5000"
-        data-infinite="false"
-        data-speed="1000"
-        data-arrows="<?=$arParams["SLIDER_ARROWS"]?>"
-        data-dots="false"
-        data-slidesToShow="<?=$arParams["LINE_ELEMENT_COUNT"]?>"
-        data-nextArrow="<a href='#' class='arrow-left text-success'><i class='fas fa-arrow-right'></i></a>"
-        data-prevArrow="<a href='#' class='arrow-right text-success'><i class='fas fa-arrow-left'></i></a>"
-    >
-        <?foreach ($arResult["ITEMS"] as $arItem) :
-            $this->AddEditAction($arItem['ID'], $arItem['EDIT_LINK'], CIBlock::GetArrayByID($arItem["IBLOCK_ID"], "ELEMENT_EDIT"));
-            $this->AddDeleteAction($arItem['ID'], $arItem['DELETE_LINK'], CIBlock::GetArrayByID($arItem["IBLOCK_ID"], "ELEMENT_DELETE"), array("CONFIRM" => GetMessage('CT_BNL_ELEMENT_DELETE_CONFIRM')));
-            ?>
-            <div id="<?=$this->GetEditAreaId($arItem['ID']);?>" class="col-lg-3 col-md-6 float-left">
-                <?$APPLICATION->IncludeComponent(
-                    "bitrix:catalog.item",
-                    '',
-                    [
-                        "RESULT" => [
-                            "ITEM" => $arItem,
-                            "OFFER_KEY" => $arItem["OFFER_ID_SELECTED"],
-                            "OFFERS_LIST" => $arItem["OFFERS"]
-                        ],
-                        "PARAMS" => [
-                            'PRICE_CODE' => $arParams['PRICE_CODE'][0]
-                        ]
-                    ],
-                    $component,
-                    ['HIDE_ICONS' => 'Y']
-                );?>
+    <section class="pt-5 pb-5">
+        <div class="container">
+            <div class="h2 mb-5">Популярные товары</div>
+            <div
+                    class="row clearfix<?=$wrapClass?>"
+                    data-autoplay="<?=$arParams["SLIDER_AUTOPLAY"]?>"
+                    data-autoplaySpeed="5000"
+                    data-infinite="false"
+                    data-speed="1000"
+                    data-arrows="<?=$arParams["SLIDER_ARROWS"]?>"
+                    data-dots="false"
+                    data-slidesToShow="<?=$arParams["LINE_ELEMENT_COUNT"]?>"
+                    data-nextArrow="<a href='#' class='arrow-left text-success'><i class='fas fa-arrow-right'></i></a>"
+                    data-prevArrow="<a href='#' class='arrow-right text-success'><i class='fas fa-arrow-left'></i></a>"
+            >
+                <?foreach ($arResult["ITEMS"] as $arItem) :
+                    $this->AddEditAction($arItem['ID'], $arItem['EDIT_LINK'], CIBlock::GetArrayByID($arItem["IBLOCK_ID"], "ELEMENT_EDIT"));
+                    $this->AddDeleteAction($arItem['ID'], $arItem['DELETE_LINK'], CIBlock::GetArrayByID($arItem["IBLOCK_ID"], "ELEMENT_DELETE"), array("CONFIRM" => GetMessage('CT_BNL_ELEMENT_DELETE_CONFIRM')));
+                    ?>
+                    <div id="<?=$this->GetEditAreaId($arItem['ID']);?>" class="col-lg-3 col-md-6 float-left">
+                        <?$APPLICATION->IncludeComponent(
+                            "bitrix:catalog.item",
+                            '',
+                            [
+                                "RESULT" => [
+                                    "ITEM" => $arItem,
+                                    "OFFER_KEY" => $arItem["OFFER_ID_SELECTED"],
+                                    "OFFERS_LIST" => $arItem["OFFERS"]
+                                ],
+                                "PARAMS" => [
+                                    'PRICE_CODE' => $arParams['PRICE_CODE'][0]
+                                ]
+                            ],
+                            $component,
+                            ['HIDE_ICONS' => 'Y']
+                        );?>
+                    </div>
+                <?endforeach;?>
             </div>
-        <?endforeach;?>
-    </div>
+        </div>
+    </section>
 <?endif?>

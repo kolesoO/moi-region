929de6ae3fb63303f558ee4fd66e12dae920ee35
kDevelop <kat-kole@mail.ru>
2020-07-22 11:25:45 +0300

    add site closed

---
+++ local/php_interface/include/site_closed.php
@@ -0,0 +1,62 @@
+<html>
+<head>
+    <title>Мы в отпуске до 1 сентября</title>
+    <style>
+        html, body {
+            margin: 0;
+            padding: 0;
+        }
+        .banner-bg {
+            background-image: url(/local/templates/common/css/../images/banner-bg.jpg);
+            background-repeat: no-repeat;
+            background-position: 50% 50%;
+            background-size: cover;
+            height: 100vh;
+            color: #fff;
+        }
+        .container {
+            width: 100%;
+            padding-right: 15px;
+            padding-left: 15px;
+            margin-right: auto;
+            margin-left: auto;
+            display: flex;
+            align-items: center;
+            justify-content: center;
+            height: 100%;
+        }
+        @media (min-width: 576px) {
+            .container {
+                max-width: 546px;
+            }
+        }
+        @media (min-width: 768px) {
+            .container {
+                max-width: 720px;
+            }
+        }
+        @media (min-width: 992px) {
+            .container {
+                max-width: 960px;
+            }
+        }
+        @media (min-width: 1200px) {
+            .container {
+                max-width: 1140px;
+            }
+        }
+        .h3 {
+            font-size: 1.75rem;
+            font-weight: 500;
+            line-height: 1.2;
+        }
+    </style>
+</head>
+<body>
+    <section class="banner-bg">
+        <div class="container">
+            <div class="h3">Мы в отпуске до 1 сентября</div>
+        </div>
+    </section>
+</body>
+</html>
